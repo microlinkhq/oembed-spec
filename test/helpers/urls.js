007f143d15a2c593990bd32713df46d5ab24ac66
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-10-21 11:52:52 +0200

    test: update

--- test/helpers/urls.js
+++ test/helpers/urls.js
@@ -20,7 +20,7 @@ const validUrls = [
   'https://www.deviantart.com/joeyjazz/art/SP-Ocean-Eclipse-781257606',
   'https://www.facebook.com/alternate.de/photos/a.391014166661/10156375231596662/?type=3&theater',
   'https://www.facebook.com/RocketBeansTV/videos/364684367488603/',
-  // 'https://www.instagram.com/farid_rueda/p/Bx-0nVPCe2c/?igshid=18g4fpv1khfug',
+  'https://www.instagram.com/p/B5LeHK2h4p0/',
   'https://www.instagram.com/p/Bx9h8mdpMPF/?utm_source=ig_web_button_share_sheet',
   'https://www.ted.com/talks/monique_w_morris_why_black_girls_are_targeted_for_punishment_at_school_and_how_to_change_that?utm_campaign=tedspread&utm_medium=referral&utm_source=tedcomshare',
   'https://www.youtube.com/watch?v=8jPQjjsBbIc',
